e5861427e8a89191ece4a4df92c1d3e908963f22
Vikky Gaurd <vikkyg@infinite.com>
2026-03-29 12:06:53 +0530

    Removed auth-service (rebuild later properly)

--- auth-service/src/main/java/com/vikky/auth/exception/InvalidPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.vikky.auth.exception;
-
-public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException(String message) {
-        super(message);
-    }
-}
